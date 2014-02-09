5d7856db08195e0b90bc5de963745c3fd4d8cad4
Jay Prakash Sarma <droidlabour@gmail.com>
2014-02-10 03:23:19 +0530

    Initial commit

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+# Specify your AWS credentials and configs below
+
+# Access key
+$access_key = '';
+# Secret access key
+$secret_key = '';
+# Region
+$region = '';
+# Autoscaling group
+$as_group = '';
+# Document root
+$doc_root = '';
